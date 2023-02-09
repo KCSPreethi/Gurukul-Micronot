546d4d38b3026994c1b434ddc9c04c512e0b1b73
KCSPreethi <preethik@sahaj.ai>
2023-02-09 12:23:02 +0530

    Add wallet limit to platform data

--- src/main/kotlin/com/tradingplatform/model/PlatformData.kt
+++ src/main/kotlin/com/tradingplatform/model/PlatformData.kt
@@ -5,7 +5,8 @@ import java.math.BigInteger
 class PlatformData {
     companion object {
         var feesEarned: BigInteger = BigInteger("0")
-        val MAX_INVENTORY_LIMIT = 10000000
+        const val MAX_INVENTORY_LIMIT = 10000000
+        const val MAX_WALLET_LIMIT = 10_000_000
 
     }
 }
