efd7a4b50ce041d567a96fc014c00bd2a0475861
KCSPreethi <preethik@sahaj.ai>
2023-02-09 17:28:10 +0530

    Remove singleton annotation

--- src/main/kotlin/com/tradingplatform/service/UserService.kt
+++ src/main/kotlin/com/tradingplatform/service/UserService.kt
@@ -6,7 +6,6 @@ import com.tradingplatform.exception.UserNotFoundException
 import com.tradingplatform.model.*
 import jakarta.inject.Singleton
 
-@Singleton
 class UserService {
     fun getUser(userName: String): User {
         return UserRepo.getUser(userName) ?: throw UserNotFoundException(listOf("user does not exists"))
