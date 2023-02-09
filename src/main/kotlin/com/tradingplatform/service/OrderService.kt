e71cbf32e81476424efca9f291e451f9b317cc22
KCSPreethi <preethik@sahaj.ai>
2023-02-09 17:40:07 +0530

    Remove redundant logic already handled by exception

--- src/main/kotlin/com/tradingplatform/service/OrderService.kt
+++ src/main/kotlin/com/tradingplatform/service/OrderService.kt
@@ -3,9 +3,7 @@ package com.tradingplatform.service
 import com.tradingplatform.data.UserRepo
 import com.tradingplatform.exception.PlaceOrderException
 import com.tradingplatform.model.*
-import com.tradingplatform.validations.OrderValidation
 import io.micronaut.http.HttpResponse
-import jakarta.inject.Singleton
 
 class OrderService {
     fun placeOrder(createOrderRequestBody: CreateOrderRequestBody, user: User): Order {
@@ -50,50 +48,23 @@ class OrderService {
 
         val totalAmount = quantity * price
         if (type == "BUY") {
-            if (totalAmount > user.wallet.getFreeAmount()) {
-                errorList.add("Insufficient funds in wallet")
-            } else if (!user.inventory.isInventoryWithinLimit(quantity)) {
-                errorList.add("Cannot place the order. Wallet amount will exceed ${PlatformData.MAX_INVENTORY_LIMIT}")
-            } else {
-
-                user.wallet.transferAmountFromFreeToLocked(totalAmount)
-                user.inventory.addESOPToCredit(quantity)
-                newOrder = Order("BUY", quantity, price, user, esopNormal)
-                user.orders.add(newOrder.id)
-
-            }
+            user.wallet.transferAmountFromFreeToLocked(totalAmount)
+            user.inventory.addESOPToCredit(quantity)
+            newOrder = Order("BUY", quantity, price, user, esopNormal)
+            user.orders.add(newOrder.id)
         } else if (type == "SELL") {
             if (esopType == "PERFORMANCE") {
-                if (quantity > user.inventory.getPerformanceFreeQuantity()) {
-                    errorList.add("Insufficient Performance ESOPs in inventory")
-                } else if (!OrderValidation().isWalletAmountWithinLimit(
-                        errorList, user, price * quantity
-                    )
-                )
-                else {
-                    user.inventory.addPerformanceESOPToLocked(quantity)
-                    user.inventory.removePerformanceESOPFromFree(quantity)
-                    user.wallet.credit += totalAmount
-
-                    newOrder = Order("SELL", quantity, price, user, esopPerformance)
-                    user.orders.add(newOrder.id)
-
-                }
+                user.inventory.addPerformanceESOPToLocked(quantity)
+                user.inventory.removePerformanceESOPFromFree(quantity)
+                user.wallet.credit += totalAmount
+                newOrder = Order("SELL", quantity, price, user, esopPerformance)
+                user.orders.add(newOrder.id)
             } else if (esopType == "NORMAL") {
-                if (quantity > user.inventory.getNormalFreeQuantity()) {
-                    errorList.add("Insufficient Normal ESOPs in inventory")
-                } else if (!OrderValidation().isWalletAmountWithinLimit(
-                        errorList, user, (price * quantity * 0.98).toInt()
-                    )
-                )
-                else {
-                    user.inventory.addNormalESOPToLocked(quantity)
-                    user.inventory.removeNormalESOPFromFree(quantity)
-                    user.wallet.credit += (totalAmount * 0.98).toInt()
-
-                    newOrder = Order("SELL", quantity, price, user, esopNormal)
-                    user.orders.add(newOrder.id)
-                }
+                user.inventory.addNormalESOPToLocked(quantity)
+                user.inventory.removeNormalESOPFromFree(quantity)
+                user.wallet.credit += (totalAmount * 0.98).toInt()
+                newOrder = Order("SELL", quantity, price, user, esopNormal)
+                user.orders.add(newOrder.id)
             }
         }
 
