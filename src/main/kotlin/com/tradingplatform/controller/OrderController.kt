b3b983bc427c1ef3f63f85ca4c32b39de5d93fa5
KCSPreethi <preethik@sahaj.ai>
2023-02-09 17:28:46 +0530

    Move OrderHandler to OrderService

--- src/main/kotlin/com/tradingplatform/controller/OrderController.kt
+++ src/main/kotlin/com/tradingplatform/controller/OrderController.kt
@@ -16,10 +16,8 @@ import javax.validation.Valid
 @Controller(value = "/user")
 class OrderController {
 
-    @Inject
-    lateinit var userService: UserService
-    @Inject
-    lateinit var orderService: OrderService
+    val userService = UserService()
+    val orderService = OrderService()
 
 
     @Get(value = "/{userName}/order")
@@ -101,101 +99,10 @@ class OrderController {
     fun createOrder(@Body @Valid createOrderRequestBody: CreateOrderRequestBody, @QueryValue userName: String): Any {
         val user = userService.getUser(userName)
         userService.canPlaceOrder(user, createOrderRequestBody)
-        val order: Order = orderService.placeOrder(createOrderRequestBody, user)
-        println(order)
-        userService.updateInventoryAndWallet(user,order)
-
-
-        //Match order os
-
-        //Update wi
-
         val quantity = createOrderRequestBody.quantity!!.toInt()
         val type = createOrderRequestBody.type!!
         val price = createOrderRequestBody.price!!.toInt()
         val esopType = createOrderRequestBody.esopType!!
-
-//        return orderHandler(userName, type, quantity, price, esopType)
-        val response = mutableMapOf<String, Any>()
-
-        response["orderId"] = order!!.id.first
-        response["quantity"] = quantity
-        response["type"] = type
-        response["price"] = price
-
-        return HttpResponse.ok(response)
+        return orderService.orderHandler(userName, type, quantity, price, esopType)
     }
-
-    fun orderHandler(userName: String, type: String, quantity: Int, price: Int, esopType: String = "NORMAL"): Any {
-        val errorList = arrayListOf<String>()
-        val response = mutableMapOf<String, Any>()
-        var newOrder: Order? = null
-
-
-        val user = UserRepo.getUser(userName)!!
-
-
-        val totalAmount = quantity * price
-        if (type == "BUY") {
-            if (totalAmount > user.wallet.getFreeAmount()) {
-                errorList.add("Insufficient funds in wallet")
-            } else if (!user.inventory.isInventoryWithinLimit(quantity)) {
-                errorList.add("Cannot place the order. Wallet amount will exceed ${PlatformData.MAX_INVENTORY_LIMIT}")
-            } else {
-
-                user.wallet.transferAmountFromFreeToLocked(totalAmount)
-                user.inventory.addESOPToCredit(quantity)
-                newOrder = Order("BUY", quantity, price, user, esopNormal)
-                user.orders.add(newOrder.id)
-
-            }
-        } else if (type == "SELL") {
-            if (esopType == "PERFORMANCE") {
-                if (quantity > user.inventory.getPerformanceFreeQuantity()) {
-                    errorList.add("Insufficient Performance ESOPs in inventory")
-                } else if (!OrderValidation().isWalletAmountWithinLimit(
-                        errorList, user, price * quantity
-                    )
-                )
-                else {
-                    user.inventory.addPerformanceESOPToLocked(quantity)
-                    user.inventory.removePerformanceESOPFromFree(quantity)
-                    user.wallet.credit += totalAmount
-
-                    newOrder = Order("SELL", quantity, price, user, esopPerformance)
-                    user.orders.add(newOrder.id)
-
-                }
-            } else if (esopType == "NORMAL") {
-                if (quantity > user.inventory.getNormalFreeQuantity()) {
-                    errorList.add("Insufficient Normal ESOPs in inventory")
-                } else if (!OrderValidation().isWalletAmountWithinLimit(
-                        errorList, user, (price * quantity * 0.98).toInt()
-                    )
-                )
-                else {
-                    user.inventory.addNormalESOPToLocked(quantity)
-                    user.inventory.removeNormalESOPFromFree(quantity)
-                    user.wallet.credit += (totalAmount * 0.98).toInt()
-
-                    newOrder = Order("SELL", quantity, price, user, esopNormal)
-                    user.orders.add(newOrder.id)
-                }
-            }
-        }
-
-        response["error"] = errorList
-        if (errorList.isNotEmpty()) {
-            return HttpResponse.badRequest(response)
-        }
-
-        response["orderId"] = newOrder!!.id.first
-        response["quantity"] = quantity
-        response["type"] = type
-        response["price"] = price
-
-        return HttpResponse.ok(response)
-    }
-
-
 }
